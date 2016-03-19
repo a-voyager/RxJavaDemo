89f813d41dabc9634565143e041d15e5b9c0c9b5
wuhaojie <w19961009@126.com>
2016-03-19 20:53:58 +0800

    add rxjava.jar & complete a demo.

--- src/top/wuhaojie/rxjavademo/Main.java
+++ src/top/wuhaojie/rxjavademo/Main.java
@@ -1,8 +1,55 @@
 package top.wuhaojie.rxjavademo;
 
+import rx.Observable;
+import rx.Observer;
+import rx.Subscriber;
+
 public class Main {
 
-	public static void main(String[] args) {
+	private Observer<String> observer = new Observer<String>() {
 
+		@Override
+		public void onCompleted() {
+			System.out.println("onCompleted");
+
+		}
+
+		@Override
+		public void onError(Throwable arg0) {
+			System.out.println("onError");
+
+		}
+
+		@Override
+		public void onNext(String arg0) {
+			System.out.println("item: " + arg0);
+
+		}
+
+	};
+
+	private Observable<String> observable = Observable
+			.create(new Observable.OnSubscribe<String>() {
+
+				@Override
+				public void call(Subscriber<? super String> s) {
+					s.onNext("Hello");
+					s.onNext("World");
+					s.onNext("!");
+					s.onCompleted();
+				}
+
+			});
+
+	/**
+	 * used for subscribe.
+	 */
+	public void begin() {
+		observable.subscribe(observer);
 	}
+
+	public static void main(String[] args) {
+		new Main().begin();
+	}
+
 }
